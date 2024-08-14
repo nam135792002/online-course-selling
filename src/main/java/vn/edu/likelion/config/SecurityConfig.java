d70d2ad70a3240dd96dfe855bd7013dd06cd19ee
RiNguyenNeverMind <namnguyenuth12012002@gmail.com>
2024-08-14 15:40:42 +0700

    hotfix cors v2

--- src/main/java/vn/edu/likelion/config/SecurityConfig.java
+++ src/main/java/vn/edu/likelion/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Arrays;
 
@@ -14,6 +15,16 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(cors->{
+            cors.configurationSource(request -> {
+                CorsConfiguration corsConfig = new CorsConfiguration();
+                corsConfig.addAllowedOrigin("*");
+                corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                corsConfig.addAllowedHeader("*");
+                return corsConfig;
+            });
+        });
+
         httpSecurity.csrf().disable()
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 
