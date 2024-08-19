c8e11906784f8460e0a1b603d4769902f37521b9
RiNguyenNeverMind <namnguyenuth12012002@gmail.com>
2024-08-19 16:48:49 +0700

    push code update api vnpay

--- src/main/java/vn/edu/likelion/service/OrderInterface.java
+++ src/main/java/vn/edu/likelion/service/OrderInterface.java
@@ -3,8 +3,6 @@ package vn.edu.likelion.service;
 import jakarta.servlet.http.HttpServletRequest;
 import vn.edu.likelion.model.payment.PaymentDTO;
 
-import java.io.UnsupportedEncodingException;
-
 public interface OrderInterface {
-    PaymentDTO createVnPayPayment(HttpServletRequest request) throws UnsupportedEncodingException;
+    PaymentDTO createVnPayPayment(HttpServletRequest request);
 }
--- src/main/java/vn/edu/likelion/service/impl/OrderServiceImpl.java
+++ src/main/java/vn/edu/likelion/service/impl/OrderServiceImpl.java
@@ -1,81 +1,36 @@
 package vn.edu.likelion.service.impl;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import vn.edu.likelion.config.VnPayConfig;
+import vn.edu.likelion.config.VNPayConfig;
 import vn.edu.likelion.model.payment.PaymentDTO;
 import vn.edu.likelion.service.OrderInterface;
+import vn.edu.likelion.utility.VNPayUtility;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderInterface {
+    private final VNPayConfig vnPayConfig;
 
     @Override
-    public PaymentDTO createVnPayPayment(HttpServletRequest request) throws UnsupportedEncodingException {
+    public PaymentDTO createVnPayPayment(HttpServletRequest request) {
         long amount = Integer.parseInt(request.getParameter("amount")) * 100L;
-        String orderType = "other";
-        System.out.println(amount);
-        String vnp_TxnRef = VnPayConfig.getRandomNumber(8);
-        String vnp_IpAddr = VnPayConfig.getIpAddress(request);
-        System.out.println(vnp_IpAddr);
-
-        String vnp_TmnCode = VnPayConfig.vnp_TmnCode;
-
-        Map<String, String> vnp_Params = new HashMap<>();
-        vnp_Params.put("vnp_Version", VnPayConfig.vnp_Version);
-        vnp_Params.put("vnp_Command", VnPayConfig.vnp_Command);
-        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
-        vnp_Params.put("vnp_Amount", String.valueOf(amount));
-        vnp_Params.put("vnp_CurrCode", "VND");
-        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-        vnp_Params.put("vnp_OrderInfo", "01");
-        vnp_Params.put("vnp_OrderType", orderType);
-        vnp_Params.put("vnp_Locale", "vn");
-        vnp_Params.put("vnp_ReturnUrl", VnPayConfig.vnp_ReturnUrl);
-        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
-
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh")); // Chuyển sang múi giờ Việt Nam
-        String vnp_CreateDate = formatter.format(cld.getTime());
-        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-
-        cld.add(Calendar.MINUTE, 10);
-        String vnp_ExpireDate = formatter.format(cld.getTime());
-        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
-
-        List fieldNames = new ArrayList(vnp_Params.keySet());
-        Collections.sort(fieldNames);
-        StringBuilder hashData = new StringBuilder();
-        StringBuilder query = new StringBuilder();
-        Iterator itr = fieldNames.iterator();
-        while (itr.hasNext()) {
-            String fieldName = (String) itr.next();
-            String fieldValue = (String) vnp_Params.get(fieldName);
-            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-                //Build hash data
-                hashData.append(fieldName);
-                hashData.append('=');
-                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                //Build query
-                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
-                query.append('=');
-                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                if (itr.hasNext()) {
-                    query.append('&');
-                    hashData.append('&');
-                }
-            }
+        String bankCode = request.getParameter("bankCode");
+        Map<String, String> vnpParamsMap = vnPayConfig.getVNPayConfig();
+        vnpParamsMap.put("vnp_Amount", String.valueOf(amount));
+        if (bankCode != null && !bankCode.isEmpty()) {
+            vnpParamsMap.put("vnp_BankCode", bankCode);
         }
-        String queryUrl = query.toString();
-        String vnp_SecureHash = VnPayConfig.hmacSHA512(VnPayConfig.secretKey, hashData.toString());
-        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
-        String paymentUrl = VnPayConfig.vnp_PayUrl + "?" + queryUrl;
-        return new PaymentDTO("ok","success",paymentUrl);
+        vnpParamsMap.put("vnp_IpAddr", VNPayUtility.getIpAddress(request));
+        //build query url
+        String queryUrl = VNPayUtility.getPaymentURL(vnpParamsMap, true);
+        String hashData = VNPayUtility.getPaymentURL(vnpParamsMap, false);
+        String vnpSecureHash = VNPayUtility.hmacSHA512(vnPayConfig.getSecretKey(), hashData);
+        queryUrl += "&vnp_SecureHash=" + vnpSecureHash;
+        String paymentUrl = vnPayConfig.getVnp_PayUrl() + "?" + queryUrl;
+        return new PaymentDTO("ok","success", paymentUrl);
     }
 }
